64ce37ad4945915317a176c899af5e5f7314501d
chenFlyingkite <30795482+chenFlyingkite@users.noreply.github.com>
2021-02-07 12:32:19 +0800

    Update SwiftAuto.java

--- src/flyingkite/SwiftAuto.java
+++ src/flyingkite/SwiftAuto.java
@@ -1,5 +1,8 @@
 package flyingkite;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SwiftAuto {
     public static void main(String[] args) {
         imports();
@@ -18,31 +21,37 @@ public class SwiftAuto {
 
     private static void toDoubles() {
         final String from = "Double";
-        String[] into = {"Int"};//, "Float", "CGFloat"};
-        String[] ops = {"+", "-", "*", "/"};
-        final String to = from;
-        ln("extension %s {", from);
-        for (String op : ops) {
-            int d = 1;
-            // public static func + (lhs: Double, rhs: Int) -> Double {
-            //     return lhs + Double(rhs);
-            // }
-            for (String ti : into) {
-                // small type +-*/ double
-                ln("%spublic static func %s (lhs: %s, rhs: %s) -> %s {", indent(d), op, from, ti, to);
-                ln("%sreturn lhs %s %s(rhs);", indent(d+1), op, from);
-                ln("%s}", indent(d));
-            }
+        String[] into = {"Int", "Int64", "Float", "CGFloat"};
+        Set<String> commt = new HashSet<>();
+        commt.add("CGFloat");
+        //String[] into = {"Int"};
+        final String[] ops = {"+", "-", "*", "/"};
+        final String returns = from;
+        final String cons = from; // constructor
+        // take the formatter of type as ts
+        int tn = from.length(); // = max(from, types)
+        for (String s : into) {
+            tn = Math.max(tn, s.length());
+        }
+        final String ts = "%" + tn + "s";
 
-            // public static func + (lhs: Int, rhs: Double) -> Double {
-            //     return Double(lhs) + rhs;
-            // }
-            for (String ti : into) {
+        ln("extension %s {", from);
+        for (String small : into) {
+            String ind = indent(1);
+            String cmm = commt.contains(small) ? "// " : "";
+            ln(ind + "// For %s", small);
+            for (String op : ops) {
                 // small type +-*/ double
-                ln("%spublic static func %s (lhs: %s, rhs: %s) -> %s {", indent(d), op, ti, from, to);
-                ln("%sreturn %s(lhs) %s rhs;", indent(d+1), from, op);
-                ln("%s}", indent(d));
+                String impl1 = "{ return %s(lhs) %s rhs; }";
+                String impl2 = "{ return lhs %s %s(rhs); }";
+                // method signature
+                String signa = "public static func %s (lhs: " + ts + ", rhs: " + ts + ") -> %s ";
+                String line1 = ind + cmm + signa + impl1;
+                String line2 = ind + cmm + signa + impl2;
+                ln(line1, op, small, from, returns, cons, op);
+                ln(line2, op, from, small, returns, op, cons);
             }
+            ln();
         }
         ln("}");
     }
